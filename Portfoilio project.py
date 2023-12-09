1c39da70d0691da5983fac33d7fa36d2cab6cf2f
Quinn <quislin77@gmail.com>
2023-12-08 22:28:59 -0500

    Added writing & reading from data.txt

--- Portfoilio project.py
+++ Portfoilio project.py
@@ -1,12 +1,9 @@
 import pandas as pd
+import math
 from decimal import Decimal
 
-class User:
-    def __init__(self, user_id):
-        self.accounts = []
-        self.user_id = user_id
-    def add_account(self, account):
-        self.accounts.append(account)
+#Personal Paper Trader 
+#Quinn Lindsey
 
 class Account:
     def __init__(self, account_id, balance):
@@ -15,101 +12,148 @@ class Account:
         self.owned_stocks = {}
     def buy_stock(self, stock, quantity):
         #if stock already owned
+        if stock.price * quantity > self.balance:
+            print("Not enough funds")
+            return
         if stock.ticker in self.owned_stocks.keys():
             #add quantity 
             self.owned_stocks[stock.ticker][1] += quantity
             self.owned_stocks[stock.ticker][2] += stock.price * quantity
             #add to total cost of stock owned
-            
         else:
             self.owned_stocks[stock.ticker] = [stock, quantity, stock.price*quantity]
             #add to total cost of stock owned
-        
+        self.balance -= stock.price * quantity
 
     def sell_stock(self, stock_ticker, quantity):
         if stock_ticker in self.owned_stocks.keys() and self.owned_stocks[stock_ticker][1] >= quantity:
             self.owned_stocks[stock_ticker][1] = self.owned_stocks[stock_ticker][1] - quantity
-            ##self.balance =+ self.owned_stocks[stock_ticker][0].price * quantity
+            stock_sell_price = get_stock_price(self.owned_stocks[stock_ticker][0].ticker)
+            self.balance += stock_sell_price * quantity
+            self.owned_stocks[stock_ticker][2] = truncate(self.owned_stocks[stock_ticker][2] - stock_sell_price * quantity, 2)
             if self.owned_stocks[stock_ticker][1] == 0:
                 self.owned_stocks.pop(stock_ticker)
         else:
             print("Stock or quantity not owned ")
-        
+    
     def __str__(self):
         print("Owned stocks: ")
         if (len(self.owned_stocks) > 0):
+            print("Cash Balance: $" + str(self.balance))
             for n in self.owned_stocks:
-                print(self.owned_stocks[n][0].ticker, "|", self.owned_stocks[n][0].company_name, "|", "Quantity:", self.owned_stocks[n][1], "|", "Total Cost:", "$" + str(self.owned_stocks[n][2]))
-            
+                print("%10s" % self.owned_stocks[n][0].ticker, end=" | ")
+                print("%48s" % self.owned_stocks[n][0].company_name, end=" | ")
+                print("%5s" % (str(self.owned_stocks[n][1])), end=" | ")
+                print("%10s" % "Total Cost: $" + str(self.owned_stocks[n][2]))
+
 class Stock:
     def __init__(self, ticker, price, company_name):
         self.company_name = company_name
         self.ticker = ticker
         self.price = price
-    
-class Hashtable:
-    def __init__(self):
-        self.table = {}
-    
-    def _hash(self, key):
-        sumH = 0
-        print(type(key))
-        for x in key:
-            sumH += ord(x)
-        return sumH
 
-    def set(self, key, value):
-        hashed_key = self._hash(key)
-        if hashed_key not in self.table:
-            self.table[hashed_key] = []
-        self.table[hashed_key] = value
-
-    def get(self, key):
-        hashed_key = self._hash(key)
-        return self.table.get(hashed_key, None)
+def get_stock_price(stock_ticker):
+    index = tickers.index(stock_ticker)
+    stock_price = float(Decimal("".join(d for d in prices[index] if d.isdigit() or d == '.')))
+    return stock_price
     
-    def delete(self, key):
-        hashed_key = self._hash(key)
-        if hashed_key in self.table:
-            del self.table[hashed_key]
+def get_stock_info(ticker):
+    index = tickers.index(ticker)
+    return Stock(ticker, prices[index], company_name[index])
+
+def truncate(number, digits) -> float:
+    Decimals = len(str(number).split('.')[1]) 
+    if Decimals <= digits:
+        return number
+    stepper = 10.0 ** digits
+    return math.trunc(stepper * number) / stepper
      
-portfolio1 = Account("ABCD", 300)
-quinn = User("QL23")
-quinn.add_account(portfolio1)
+def get_account(id):
+    line = None
+    account = None
+    with open("data.txt") as file:
+        while True:
+            line = file.readline()
+            if id == line[:line.index(";")]:
+                break
+            if not(line):
+                print("Account not found, try again.")
+                exit()
+            
+        balance = line[line.index(";")+1:line.index("-")]
+        stocks_line = line[line.index("-")+1:]
+        account = Account(id, float(balance))
+        while len(stocks_line) > 1:
+            stock = stocks_line[:stocks_line.index(",")]
+            ticker = stock[:stock.index(":")]
+            quantity = int(stock[stock.index(":")+1:])
+            stocks_line = stocks_line[len(stock)+1:]
+            account.owned_stocks[ticker] = [get_stock_info(ticker), quantity, truncate(float(quantity) * get_stock_price(ticker), 2)]
+    return account
 
+def save_account(account):
+    line = None
+    lines = None
+    data = ""
+
+    data = str(account.account_id) + ";" + str(truncate(account.balance, 2)) + "-"
+    for n in account.owned_stocks:
+        data += account.owned_stocks[n][0].ticker + ":" + str(account.owned_stocks[n][1]) + ","
+    
+    #Find line that contains this account
+    with open("data.txt", "r") as file:
+        lines = file.readlines()
+        counter = 0
+        while True:
+            line = lines[counter][:lines[counter].index(";")]
+            if account.account_id in line:
+                break
+            counter += 1
+        lines[counter] = data + "\n"
+    
+    #Write data
+    with open("data.txt", "w") as file:
+        for line in lines:
+            file.write(line)
 
 #Reading CSV data into a dictionary
-#Turn this into hashmap later
-#Automate downloading updated CSV hourly from https://www.nasdaq.com/market-activity/stocks/screener
+#Eventually Automate downloading updated CSV hourly from https://www.nasdaq.com/market-activity/stocks/screener
 stocks = pd.read_csv("stocks.csv")
 tickers = list(stocks["Symbol"])
 prices = list(stocks["Last Sale"])
 company_name = list(stocks["Name"])
 
-hashtbl = Hashtable()
-for x in tickers:
-    if type(x) == 'str':
-        hashtbl.set(x, prices[tickers.index(x)])
 
 #Interface    
 num = 0
-while num < 5:
-    num = int(input("Enter 2 to buy a stock, 3 to sell a stock, 4 to display account details, 5 to open a new account, 6 to sign in to an account"))              
+#Enter ABC for test account
+login_id = input("Enter account ID (use ABC for testing) to login: ")
+account = get_account(login_id)
+
+while num < 6:
+    num = int(input("Enter 2 to buy a stock, 3 to sell a stock, 4 to display account details, 5 to exit and save: "))              
     if num == 2:
         ticker = input("Enter stock ticker to buy: ")
-        #Searching through tickers list to find that stock index, faster with hashmap.
         stock_index = tickers.index(ticker)
         price = float(Decimal("".join(d for d in prices[stock_index] if d.isdigit() or d == '.')))
         stock = Stock(tickers[stock_index], price, company_name[stock_index])
-        portfolio1.buy_stock(stock, int(input("Enter quantity: ")))
-        
+        account.buy_stock(stock, int(input("Enter quantity: ")))
     if num == 3:
-        portfolio1.sell_stock(input("Enter stock ticker to sell: "), int(input("Enter quantity to sell: ")))
+        account.sell_stock(input("Enter stock ticker to sell: "), int(input("Enter quantity to sell: ")))
     if num == 4:
-        portfolio1.__str__()
+        account.__str__()
     if num == 5:
-        user_id = input("Enter user id: ")
-        password = input("Enter password: ")
-    if num == 6:
-        user_id = input("Enter user id: ")
-        password = input("Enter password: ")
+        save_account(account)
+        exit()
+  
+#Unit Tests
+assert type(account.balance) is float
+assert type(account.account_id) is str
+assert len(stocks) > 0
+assert len(tickers) > 0
+assert len(prices) > 0
+assert len(company_name) > 0
+assert login_id == account.account_id
+assert num < 6
+assert get_stock_info("T").ticker == "T"
+assert get_stock_price("T") == get_stock_info("T").price
